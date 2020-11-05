b5cfd2d5fb482888be39f6f4d93a1e1d7907e58c
Oleksandr Bedenko <oleksandr.bedenko@gmail.com>
2020-11-05 18:51:14 +0300

    try node-telegram-bot-api

--- index.js
+++ index.js
@@ -12,27 +12,12 @@ const config = {
 const bot = new TelegramBot(token, config);
 bot.setWebHook(url);
 
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  console.log(msg);
-
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
-
-  const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
-
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
-});
-
 // Listen for any kind of message. There are different kinds of
 // messages.
 bot.on('message', (msg) => {
-  console.log(msg);
+  // console.log('message' msg);
   const chatId = msg.chat.id;
 
   // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message');
+  bot.sendMessage(chatId, `Hello ${msg.from?.first_name}, I've received your message`);
 });
