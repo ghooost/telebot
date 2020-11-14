62acc97f6095740a81e7549a65cee66ef170d19b
Oleksandr Bedenko <oleksandr.bedenko@gmail.com>
2020-11-14 15:05:34 +0300

    env

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const TelegramBot = require('node-telegram-bot-api');
 
 // replace the value below with the Telegram token you receive from @BotFather
 const token = process.env.BOT_TOKEN;
-const port = process.env.PORT || 8443;
+const port = heroku  || 8443;
 const host = process.env.HOST;
 const url = `https://dry-eyrie-81555.herokuapp.com:/${token}`;
 const config = {
@@ -19,5 +19,5 @@ bot.on('message', (msg) => {
   const chatId = msg.chat.id;
 
   // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, `Hello ${msg.from.first_name}, I've received your message`);
+  bot.sendMessage(chatId, `Hello ${msg.from.first_name}, I've received your message and env are ${JSON.stringify(process.env, '', 2)}`);
 });
